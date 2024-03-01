72e5b62a365d842d5cfc847994ea751c7dfce3e2
Michael Tamsil <michaeltamsil@gmaill.com>
2024-03-01 13:38:05 +0700

    210. A Form-aware Dropdown Button

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
-class NewItem extends StatefulWidget{
+class NewItem extends StatefulWidget {
   const NewItem({super.key});
-  
+
   @override
   State<NewItem> createState() {
     return _NewItemState();
@@ -18,7 +19,61 @@ class _NewItemState extends State<NewItem> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Text('The form')
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  label: Text('Name'),
+                ),
+                maxLength: 50,
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ), // instead of TextField()
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  color: category.value.color,
+                                  height: 16,
+                                  width: 16,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          )
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
