2dff4616adccb5aeb616e5e595ddb74a834ba797
Michael Tamsil <michaeltamsil@gmaill.com>
2024-03-07 09:35:15 +0700

    229. Sending DELETE Requests

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -86,10 +86,25 @@ class _GroceryListState extends State<GroceryList> {
     
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+
+    final url = Uri.https(
+        'flutter-prep-4ecb6-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping-list/${item.id}.json');
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+
+    
   }
 
   @override
